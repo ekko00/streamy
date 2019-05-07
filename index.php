ae7eb719723f1ed7da28094b7f3a889641bebc0e
FlorentMTT <47171977+FlorentMTT@users.noreply.github.com>
2019-05-07 09:33:55 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,48 +1,30 @@
 <?php
-$temp = filter_input(INPUT_POST, 'file');
-if ($temp) {
 
-$name= $_FILES['file']['name'];
+if (isset($_FILES['fileToSend'])) {
+  $name= $_FILES['fileToSend']['name'];
+  $tmp_name= $_FILES['fileToSend']['tmp_name'];
+  $position= strpos($name, ".");
+  $fileextension= substr($name, $position + 1);
+  $fileextension= strtolower($fileextension) . 'test';
 
-$tmp_name= $_FILES['file']['tmp_name'];
+  if (isset($name)) {
+  $path = './uploaded/';
+  if (empty($name))
+  {
+    echo "Please choose a file";
+  }
+  else if (!empty($name)){
+    if (($fileextension != "mp4") && ($fileextension != "ogg") && ($fileextension != "webm")) {
+      echo "The file extension must be .mp4, .ogg, or .webm in order to be uploaded";
+    }
+    else if (($fileextension == "mp4") || ($fileextension == "ogg") || ($fileextension == "webm"))
+    {
+      if (move_uploaded_file($tmp_name, $path.$name)) {
+        echo 'Uploaded!';
+      }
+    }
 
-$position= strpos($name, ".");
-
-$fileextension= substr($name, $position + 1);
-
-$fileextension= strtolower($fileextension);
-
-
-if (isset($name)) {
-
-$path= './test/';
-if (empty($name))
-{
-echo "Please choose a file";
-}
-else if (!empty($name)){
-if (($fileextension !== "mp4") && ($fileextension !== "ogg") && ($fileextension !== "webm"))
-echo "The file extension must be .mp4, .ogg, or .webm in order to be uploaded";
-}
-
-
-else if (($fileextension == "mp4") || ($fileextension == "ogg") || ($fileextension == "webm"))
-{
-if (move_uploaded_file($tmp_name, $path.$name)) {
-echo 'Uploaded!';
-}
-}
-}
-
-if (($fileextension == "mp4") || ($fileextension == "ogg") || ($fileextension == "webm"))
-{
-echo "<video width='320' controls>
-<source src='$path/$name' type='video/$fileextension'>
-Votre explorateur ne supporte pas les tags vidéos.
-</video>";
-// Your browser does not support the video tag.
-}
-}
+  }
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -52,9 +34,8 @@ Votre explorateur ne supporte pas les tags vidéos.
   </head>
   <body>
     <form action="#" method='post' enctype="multipart/form-data">
-<input type="file" name="file"/><br><br>
+<input type="file" name="fileToSend"/><br><br>
 	<input type="submit" value="Upload"/>
-</form>
 </form>
   </body>
 </html>
