1c69b913a69653256095598347d9d6078f487ca8
FlorentMTT <47171977+FlorentMTT@users.noreply.github.com>
2019-05-14 11:32:56 +0200

    Add files via upload

---
+++ UploadFile/index.php
@@ -0,0 +1,43 @@
+<?php
+if (isset($_FILES['fileToSend'])) {
+  print 1;
+  $name= $_FILES['fileToSend']['name'];
+  $tmp_name= $_FILES['fileToSend']['tmp_name'];
+  $position= strpos($name, ".");
+  $fileextension= substr($name, $position + 1);
+  print $fileextension= strtolower($fileextension);
+
+  if (isset($name)) {
+  $path = './uploaded/';
+  if (empty($name))
+  {
+    echo "Please choose a file";
+  }
+  else if (!empty($name)){
+    if (($fileextension != "mp4") && ($fileextension != "ogg") && ($fileextension != "webm")) {
+      echo "The file extension must be .mp4, .ogg, or .webm in order to be uploaded";
+    }
+    else if (($fileextension == "mp4") || ($fileextension == "ogg") || ($fileextension == "webm"))
+    {
+      if (move_uploaded_file($tmp_name, $path.$name)) {
+        echo ' Uploaded!';
+      }
+    }
+
+  }
+}
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Streamy</title>
+  </head>
+  <body>
+    <form action="#" method='post' enctype="multipart/form-data">
+      <input type="file" name="fileToSend"/><br><br>
+	<input type="submit" value="Upload"/>
+</form>
+  </body>
+</html>
